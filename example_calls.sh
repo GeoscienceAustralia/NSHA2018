65df9c346301d06a95eda01e698fc668842fc0a5
griffij <jono.d.griffin@gmail.com>
2016-07-01 13:15:22 +1000

    Updated example call and fault slip rate converter  Please enter the commit message for your changes. Lines starting

--- example_calls.sh
+++ example_calls.sh
@@ -2,4 +2,8 @@
 python shapefile_2_complexfault.py -shapefile SUMATRA/sumatra.shp -shapefile_depth_attribute level
 
 # More complex
+# Complex fault example
 python shapefile_2_complexfault.py -shapefile SUMATRA/sumatra.shp -shapefile_depth_attribute level -source_model_name SUMATRA_1234 -complex_fault_id 1234 -complex_fault_name sumatra_GAR_version -complex_fault_tectonic_region SUMATRA -magnitude_scaling_relation WC1994 -rupture_aspect_ratio 2.0 -a_value 20 -b_value 1.0 -min_mag 4.5 -max_mag 9.7 -rake 90 -output_dir nrml
+
+# Simple fault example
+python shapefile_2_simplefault.py -shapefile ADELAIDE/Adelaide_simple_faults.shp -shapefile_faultname_attribute Name -shapefile_dip_attribute Dip -shapefile_sliprate_attribute Slip_Rate -source_model_name Adelaide_faults -simple_fault_tectonic_region South_Australia -magnitude_scaling_relation WC1994 -rupture_aspect_ratio 1.5 -upper_depth 0.001 -lower_depth 20 -a_value 1.0 -b_value 1.0 -min_mag 4.5 -max_mag 7.0 -rake 90 -output_dir Adelaide_test
