655738e7189b4ad686b7867b6273860c126ac0f9
griffij <jono.d.griffin@gmail.com>
2017-01-20 13:09:07 +1100

    Adding script to convert shapefile to nrml for maximum magnitude distribution

--- source_models/faults/shapefile2nrml/example_calls.sh
+++ source_models/faults/shapefile2nrml/example_calls.sh
@@ -10,3 +10,6 @@ python shapefile_2_simplefault.py -shapefile ADELAIDE/Adelaide_simple_faults.shp
 
 # Simple fault CE example (note max_mag here means characteristic magnitude)
 python shapefile_2_simplefault_CE.py -shapefile ADELAIDE/Adelaide_simple_faults.shp -shapefile_faultname_attribute Name -shapefile_dip_attribute Dip -shapefile_sliprate_attribute Slip_Rate -source_model_name Adelaide_faults -simple_fault_tectonic_region Non_cratonic -magnitude_scaling_relation WC1994 -rupture_aspect_ratio 1.5 -upper_depth 0.001 -lower_depth 20 -a_value 1.0 -b_value 1.0 -min_mag 4.5 -max_mag 7.0 -rake 90 -output_dir Adelaide_test_CE -incremental_mfd True
+
+# Simple fault MM example (maximum magnitude model)
+python shapefile_2_simplefault_MM.py -shapefile faults/cadell_fault.shp -shapefile_faultname_attribute Name -shapefile_dip_attribute Dip -shapefile_sliprate_attribute SL_RT_LT -source_model_name Cadell_fault -simple_fault_tectonic_region Non_cratonic -magnitude_scaling_relation WC1994 -rupture_aspect_ratio 1.5 -upper_depth 0.001 -lower_depth 20 -a_value 1.0 -b_value 1.0 -min_mag 4.5 -max_mag 7.0 -rake 90 -output_dir Cadell_MM -incremental_mfd True
