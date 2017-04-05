da66c5b7f6d919698a8c2a81c31b8dfacc746f94
griffij <jono.d.griffin@gmail.com>
2017-04-05 13:28:22 +1000

    Now processes ground motion model results too

--- source_models/logic_trees/build_logic_tree_weights.py
+++ source_models/logic_trees/build_logic_tree_weights.py
@@ -13,16 +13,19 @@ import scipy.io
 import matplotlib.pyplot as plt
 
 num_ssc_experts = 15
-num_gm_experts = 10
+num_gmm_experts = 10
 
+#########################################################
+# Seismic source model
 calib_path = '/nas/gemd/ehp/georisk_earthquake/hazard/NSHM_18/Expert_Elicitation/Excalibur/data/'
 ssc_weights_file = join(calib_path, 'source.calib.files', 'source.model.calib.weight') #'sourceCalibWeights_format7.m')
 
 ssc_weights = scipy.io.loadmat(ssc_weights_file)
+#print ssc_weights
 ssc_weights = np.array(ssc_weights['calibration_weight']).flatten()
 #print ssc_weights
 
-# Load in target responses
+# Load in target responses for seismic source mode,l
 target_path = '/nas/gemd/ehp/georisk_earthquake/hazard/NSHM_18/Expert_Elicitation/Target_questions'
 fig_path =join(target_path, 'seismic_source_results', 'Figures')
 ssc_weights
@@ -54,6 +57,9 @@ for line in f_in.readlines():
     ids.append(line.split(',')[0])
 print ids
 
+######################################################
+# Utility functions
+
 def get_weights(q_list, weighted_sum):
     ind = []
     for q in q_list:
@@ -72,20 +78,25 @@ def autolabel(rects, ax, fontsize = 12):
                 '%.3f' % height,
                 ha='center', va='bottom', fontsize = fontsize)
 
-def bar_plot(q_list, weight_list, label_list, filename, title, fontsize = 12, fig_path = fig_path):
+def bar_plot(q_list, weight_list, label_list, filename, title, fontsize = 12,
+             xlabelfont = None, fig_path = fig_path, wide=False):
     width = 0.35
     x_vals = np.arange(len(q_list))
     fig, ax = plt.subplots()
     rects1 = ax.bar(x_vals, weight_list, width, color='b')
-    
+    if xlabelfont is None:
+        xlabelfont = fontsize
     ax.set_ylabel('Weight')
     ax.set_title(title)
     ax.set_xticks(x_vals + width / 2)
-    ax.set_xticklabels(label_list)
+    ax.set_xticklabels(label_list, fontsize=xlabelfont)
     ax.set_ylim([0,1])
     ax.set_xlim([-0.5, (len(q_list))])
-    autolabel(rects1, ax,fontsize = fontsize )
-   
+    autolabel(rects1, ax,fontsize = fontsize)
+    if wide:
+        width, height = fig.get_size_inches()
+        fig.set_size_inches(width*wide, height)
+    plt.tight_layout()
     plt.savefig(join(fig_path, filename))
 
 def bar_subplots(q_lists, weight_lists, label_lists, filename, 
@@ -111,6 +122,9 @@ def bar_subplots(q_lists, weight_lists, label_lists, filename,
         autolabel(rects1, ax, fontsize = fontsize)
     plt.tight_layout()
     plt.savefig(join(fig_path, filename))
+
+######################################################
+# Calculate and plot seismic source model logic tree weights
 
 # Source type
 src_type =['S2Q1', 'S2Q2', 'S2Q3', 'S2Q4', 'S2Q5']
@@ -260,3 +274,276 @@ mfd_title_list = ['Cratonic', 'Non-cratonic', 'Extended']
 bar_subplots(mfd_qlists, mfd_weight_lists, mfd_label_lists, \
              'fault_mfd_integration_weights.png', mfd_title_list, \
              num_row = 3, num_col = 1, fontsize = 10)
+
+######################################################
+# Calculate and plot ground motion logic tree weights
+# Load data
+calib_path = '/nas/gemd/ehp/georisk_earthquake/hazard/NSHM_18/Expert_Elicitation/Excalibur/data/GMPE/'
+gmm_weights_file = join(calib_path, 'ground.motion.model.calib.weight.p0.3.mat') #'sourceCalibWeights_format7.m')
+
+gmm_weights = scipy.io.loadmat(gmm_weights_file)
+#print gmm_weights
+gmm_weights = np.array(gmm_weights['gmm_calibration_weight']).flatten()
+#print gmm_weights
+
+# Load in target responses for ground motion model
+print 'Loading ground motion model weight data'
+target_path = '/nas/gemd/ehp/georisk_earthquake/hazard/NSHM_18/Expert_Elicitation/Target_questions'
+fig_path =join(target_path, 'ground_motion_results', 'Figures')
+#gmm_weights
+gmm_responses = {}
+for i in range(num_gmm_experts):
+    print 'Reading response from expert', i+1
+    filename = 'Results_expert%i.csv' % (i+1)
+    filepath = join(target_path, 'ground_motion_results', filename)
+    try:
+        data = np.genfromtxt(filepath, delimiter = ',', skip_header = 1, usecols = (1,2,3))#, filling_values = 0, missing_values = 0)
+    except IOError:
+        print 'Missing file from expert %i!!!' % (i+1)
+        continue
+    # Check for missing responses
+    if np.isnan(data).any():
+        print 'Missing values for expert  %i!!!' % (i+1)
+        print 'Currently setting all nan to zero'
+        data = np.nan_to_num(data)
+    weighted_data = data*gmm_weights[i]
+    gmm_responses[str(i+1)] = weighted_data
+weighted_sum = sum(gmm_responses.values())
+#print weighted_sum
+
+# get question ids
+ids = []
+f_in = open(filepath, 'r')
+header = f_in.readline()
+for line in f_in.readlines():
+    ids.append(line.split(',')[0])
+print ids
+
+c_gmm_model_all_w = []
+c_gmm_model_all_labels = []
+nc_ex_gmm_model_all_w = []
+nc_ex_gmm_model_all_labels = []
+banda_gmm_model_all_w = []
+banda_gmm_model_all_labels = []
+
+gmm_fig_path = fig_path =join(target_path, 'ground_motion_results', 'Figures')
+# Cratonic GMM type
+c_gmm_region_qlist = ['S1Q1', 'S1Q2', 'S1Q3', 'S1Q4']
+c_gmm_region_labels = ['Australian', 'CEUS', 'California', 'European']
+c_gmm_region_w = get_weights(c_gmm_region_qlist, weighted_sum)
+print c_gmm_region_w, sum(c_gmm_region_w)
+bar_plot(c_gmm_region_qlist, c_gmm_region_w, c_gmm_region_labels, \
+         'cratonic_gmm_region_weights.png', \
+         'Cratonic GMM Region Weights', fig_path = gmm_fig_path)
+
+# Non-cratonic and extended GMM type
+nc_ex_gmm_region_qlist = ['S1Q5', 'S1Q6', 'S1Q7', 'S1Q8']
+nc_ex_gmm_region_labels = ['Australian', 'CEUS', 'California', 'European']
+nc_ex_gmm_region_w = get_weights(nc_ex_gmm_region_qlist, weighted_sum)
+print nc_ex_gmm_region_w, sum(nc_ex_gmm_region_w)
+bar_plot(nc_ex_gmm_region_qlist, nc_ex_gmm_region_w, nc_ex_gmm_region_labels, \
+         'non_cratonic_extended_gmm_region_weights.png', \
+         'Non-cratonic and Extended GMM Region Weights', fig_path = gmm_fig_path)
+
+# Banda Sea GMM type
+banda_gmm_region_qlist = ['S1Q9', 'S1Q10', 'S1Q11', 'S1Q12', 'S1Q13']
+banda_gmm_region_labels = ['Australian', 'CEUS', 'California', 'European','Subduction \nIntraslab']
+banda_gmm_region_w = get_weights(banda_gmm_region_qlist, weighted_sum)
+print banda_gmm_region_w, sum(banda_gmm_region_w)
+bar_plot(banda_gmm_region_qlist, banda_gmm_region_w, banda_gmm_region_labels, \
+         'banda_sea_gmm_region_weights.png', \
+         'Banda Sea GMM Region Weights', fig_path = gmm_fig_path)
+
+##############
+# Cratonic models
+# Cratonic GMM Australian models
+c_gmm_aust_qlist = ['S2Q1', 'S2Q2', 'S2Q3', 'S2Q4']
+c_gmm_aust_labels = ['Allen2012', 'Allen2012 \n RedSigma', 'Som2009 \n Non-cratonic', 'Som2009 \n Yilgarn']
+c_gmm_aust_w = get_weights(c_gmm_aust_qlist, weighted_sum)
+print c_gmm_aust_w, sum(c_gmm_aust_w)
+c_gmm_aust_region_w = c_gmm_aust_w *c_gmm_region_w[0]
+c_gmm_model_all_w += list(c_gmm_aust_region_w)
+c_gmm_model_all_labels += c_gmm_aust_labels
+bar_plot(c_gmm_aust_qlist, c_gmm_aust_w, c_gmm_aust_labels, \
+         'cratonic_gmm_australian_weights.png', \
+         'Cratonic Australian GMM Weights', fig_path = gmm_fig_path)
+
+# Cratonic GMM CEUS models
+c_gmm_ceus_qlist = ['S2Q5', 'S2Q6', 'S2Q7', 'S2Q8', 'S2Q9', 'S2Q10', 'S2Q11']
+c_gmm_ceus_labels = ['AtkBoore\n2006', 'AtkBoore\n2006\nMod2011', 'Campbell\n2003', 'Pezeshk\n2011', 'Silva2002\nMwNSHMP\n2008', 'Toro\n2002', 'YenAtk\n2015']
+c_gmm_ceus_w = get_weights(c_gmm_ceus_qlist, weighted_sum)
+print c_gmm_ceus_w, sum(c_gmm_ceus_w)
+c_gmm_ceus_region_w = c_gmm_ceus_w *c_gmm_region_w[1]
+c_gmm_model_all_w += list(c_gmm_ceus_region_w)
+c_gmm_model_all_labels += c_gmm_ceus_labels
+bar_plot(c_gmm_ceus_qlist, c_gmm_ceus_w, c_gmm_ceus_labels, \
+         'cratonic_gmm_CEUS_weights.png', \
+         'Cratonic CEUS GMM Weights', fig_path = gmm_fig_path)
+
+# Cratonic GMM California models
+c_gmm_cal_qlist = ['S2Q12', 'S2Q13', 'S2Q14']
+c_gmm_cal_labels = ['Boore2014', 'ChiYou2008', 'ChiYou2014']
+c_gmm_cal_w = get_weights(c_gmm_cal_qlist, weighted_sum)
+print c_gmm_cal_w, sum(c_gmm_cal_w)
+c_gmm_cal_region_w = c_gmm_cal_w *c_gmm_region_w[2]
+c_gmm_model_all_w += list(c_gmm_cal_region_w)
+c_gmm_model_all_labels += c_gmm_cal_labels
+bar_plot(c_gmm_cal_qlist, c_gmm_cal_w, c_gmm_cal_labels, \
+         'cratonic_gmm_california_weights.png', \
+         'Cratonic Californian GMM Weights', fig_path = gmm_fig_path)
+
+# Cratonic GMM Eurpoean models
+c_gmm_eur_qlist = ['S2Q15', 'S2Q16', 'S2Q17']
+c_gmm_eur_labels = ['ChiYo2008\nSWISS01', 'Rietbrock\n2013SS', 'Zhao2006\nAscSWISS05']
+c_gmm_eur_w = get_weights(c_gmm_eur_qlist, weighted_sum)
+print c_gmm_eur_w, sum(c_gmm_eur_w)
+c_gmm_eur_region_w = c_gmm_eur_w *c_gmm_region_w[3]
+c_gmm_model_all_w += list(c_gmm_eur_region_w)
+c_gmm_model_all_labels += c_gmm_eur_labels
+bar_plot(c_gmm_eur_qlist, c_gmm_eur_w, c_gmm_eur_labels, \
+         'cratonic_gmm_european_weights.png', \
+         'Cratonic Eurpean GMM Weights', fig_path = gmm_fig_path)
+
+##############
+# Non-cratonic and Extended models
+# Non-cratonic and Extended GMM Australian models
+nc_ex_gmm_aust_qlist = ['S3Q1', 'S3Q2', 'S3Q3', 'S3Q4']
+nc_ex_gmm_aust_labels = ['Allen2012', 'Allen2012 \n RedSigma', 'Some2009 \n Non-cratonic', 'Som2009 \n Yilgarn']
+nc_ex_gmm_aust_w = get_weights(nc_ex_gmm_aust_qlist, weighted_sum)
+print nc_ex_gmm_aust_w, sum(nc_ex_gmm_aust_w)
+nc_ex_gmm_aust_region_w = nc_ex_gmm_aust_w *nc_ex_gmm_region_w[0]
+nc_ex_gmm_model_all_w += list(nc_ex_gmm_aust_region_w)
+nc_ex_gmm_model_all_labels += nc_ex_gmm_aust_labels
+bar_plot(nc_ex_gmm_aust_qlist, nc_ex_gmm_aust_w, nc_ex_gmm_aust_labels, \
+         'nc_ex_gmm_australian_weights.png', \
+         'Non-cratonic and Extended Australian GMM Weights', fig_path = gmm_fig_path)
+
+# Non-cratonic and Extended GMM CEUS models
+nc_ex_gmm_ceus_qlist = ['S3Q5', 'S3Q6', 'S3Q7', 'S3Q8', 'S3Q9', 'S3Q10', 'S3Q11']
+nc_ex_gmm_ceus_labels = ['AtkBoore\n2006', 'AtkBoore\n2006\nMod2011', 'Campbell\n2003', 'Pezeshk\n2011', 'Silva2002\nMwNSHMP\n2008', 'Toro\n2002', 'YenAtk\n2015']
+nc_ex_gmm_ceus_w = get_weights(nc_ex_gmm_ceus_qlist, weighted_sum)
+print nc_ex_gmm_ceus_w, sum(nc_ex_gmm_ceus_w)
+nc_ex_gmm_ceus_region_w = nc_ex_gmm_ceus_w *nc_ex_gmm_region_w[1]
+nc_ex_gmm_model_all_w += list(nc_ex_gmm_ceus_region_w)
+nc_ex_gmm_model_all_labels += nc_ex_gmm_ceus_labels
+bar_plot(nc_ex_gmm_ceus_qlist, nc_ex_gmm_ceus_w, nc_ex_gmm_ceus_labels, \
+         'nc_ex_gmm_CEUS_weights.png', \
+         'Non-cratonic and Extended CEUS GMM Weights', fig_path = gmm_fig_path)
+
+# Non-cratonic and Extended GMM California models
+nc_ex_gmm_cal_qlist = ['S3Q12', 'S3Q13', 'S3Q14']
+nc_ex_gmm_cal_labels = ['Boore2014', 'ChiYou2008', 'ChiYou2014']
+nc_ex_gmm_cal_w = get_weights(nc_ex_gmm_cal_qlist, weighted_sum)
+print nc_ex_gmm_cal_w, sum(nc_ex_gmm_cal_w)
+nc_ex_gmm_cal_region_w = nc_ex_gmm_cal_w *nc_ex_gmm_region_w[2]
+nc_ex_gmm_model_all_w += list(nc_ex_gmm_cal_region_w)
+nc_ex_gmm_model_all_labels += nc_ex_gmm_cal_labels
+bar_plot(nc_ex_gmm_cal_qlist, nc_ex_gmm_cal_w, nc_ex_gmm_cal_labels, \
+         'nc_ex_gmm_california_weights.png', \
+         'Non-cratonic and Extended Californian GMM Weights', fig_path = gmm_fig_path)
+
+# Non-cratonic and Extended GMM Eurpoean models
+nc_ex_gmm_eur_qlist = ['S3Q15', 'S3Q16', 'S3Q17']
+nc_ex_gmm_eur_labels = ['ChiYo2008\nSWISS01', 'Rietbrock\n2013SS', 'Zhao2006\nAscSWISS05']
+nc_ex_gmm_eur_w = get_weights(nc_ex_gmm_eur_qlist, weighted_sum)
+print nc_ex_gmm_eur_w, sum(nc_ex_gmm_eur_w)
+nc_ex_gmm_eur_region_w = nc_ex_gmm_eur_w *nc_ex_gmm_region_w[3]
+nc_ex_gmm_model_all_w += list(nc_ex_gmm_eur_region_w)
+nc_ex_gmm_model_all_labels += nc_ex_gmm_eur_labels
+bar_plot(nc_ex_gmm_eur_qlist, nc_ex_gmm_eur_w, nc_ex_gmm_eur_labels, \
+         'nc_ex_gmm_european_weights.png', \
+         'Non-cratonic and Extended Eurpean GMM Weights', fig_path = gmm_fig_path)
+
+##############
+# Banda Sea models
+# Banda Sea GMM Australian models
+banda_gmm_aust_qlist = ['S4Q1', 'S4Q2', 'S4Q3', 'S4Q4']
+banda_gmm_aust_labels = ['Allen2012', 'Allen2012 \n RedSigma', 'Som2009 \n Non-cratonic', 'Som2009 \n Yilgarn']
+banda_gmm_aust_w = get_weights(banda_gmm_aust_qlist, weighted_sum)
+print banda_gmm_aust_w, sum(banda_gmm_aust_w)
+banda_gmm_aust_region_w = banda_gmm_aust_w *banda_gmm_region_w[0]
+banda_gmm_model_all_w += list(banda_gmm_aust_region_w)
+banda_gmm_model_all_labels += banda_gmm_aust_labels
+bar_plot(banda_gmm_aust_qlist, banda_gmm_aust_w, banda_gmm_aust_labels, \
+         'banda_gmm_australian_weights.png', \
+         'Banda Sea Australian GMM Weights', fig_path = gmm_fig_path)
+
+# Banda Sea GMM CEUS models
+banda_gmm_ceus_qlist = ['S4Q5', 'S4Q6', 'S4Q7', 'S4Q8', 'S4Q9', 'S4Q10', 'S4Q11']
+banda_gmm_ceus_labels = ['AtkBoore\n2006', 'AtkBoore\n2006\nMod2011', 'Campbell\n2003', 'Pezeshk\n2011', 'Silva2002\nMwNSHMP\n2008', 'Toro\n2002', 'YenAtk\n2015']
+banda_gmm_ceus_w = get_weights(banda_gmm_ceus_qlist, weighted_sum)
+print banda_gmm_ceus_w, sum(banda_gmm_ceus_w)
+banda_gmm_ceus_region_w = banda_gmm_ceus_w *banda_gmm_region_w[1]
+banda_gmm_model_all_w += list(banda_gmm_ceus_region_w)
+banda_gmm_model_all_labels += banda_gmm_ceus_labels
+bar_plot(banda_gmm_ceus_qlist, banda_gmm_ceus_w, banda_gmm_ceus_labels, \
+         'banda_gmm_CEUS_weights.png', \
+         'Banda Sea CEUS GMM Weights', fig_path = gmm_fig_path)
+
+# Banda Sea GMM California models
+banda_gmm_cal_qlist = ['S4Q12', 'S4Q13', 'S4Q14']
+banda_gmm_cal_labels = ['Boore2014', 'ChiYou2008', 'ChiYou2014']
+banda_gmm_cal_w = get_weights(banda_gmm_cal_qlist, weighted_sum)
+print banda_gmm_cal_w, sum(banda_gmm_cal_w)
+banda_gmm_cal_region_w = banda_gmm_cal_w *banda_gmm_region_w[2]
+banda_gmm_model_all_w += list(banda_gmm_cal_region_w)
+banda_gmm_model_all_labels += banda_gmm_cal_labels
+bar_plot(banda_gmm_cal_qlist, banda_gmm_cal_w, banda_gmm_cal_labels, \
+         'banda_gmm_california_weights.png', \
+         'Banda Sea Californian GMM Weights', fig_path = gmm_fig_path)
+
+# Banda Sea GMM Eurpoean models
+banda_gmm_eur_qlist = ['S4Q15', 'S4Q16', 'S4Q17']
+banda_gmm_eur_labels = ['ChiYo2008\nSWISS01', 'Rietbrock\n2013SS', 'Zhao2006\nAscSWISS05']
+banda_gmm_eur_w = get_weights(banda_gmm_eur_qlist, weighted_sum)
+print banda_gmm_eur_w, sum(banda_gmm_eur_w)
+banda_gmm_eur_region_w = banda_gmm_eur_w *banda_gmm_region_w[3]
+banda_gmm_model_all_w += list(banda_gmm_eur_region_w)
+banda_gmm_model_all_labels += banda_gmm_eur_labels
+bar_plot(banda_gmm_eur_qlist, banda_gmm_eur_w, banda_gmm_eur_labels, \
+         'banda_gmm_european_weights.png', \
+         'Banda Sea Eurpean GMM Weights', fig_path = gmm_fig_path)
+
+# Banda Sea GMM intraslab models
+banda_gmm_inslab_qlist = ['S4Q18', 'S4Q19', 'S4Q20', 'S4Q21']
+banda_gmm_inslab_labels = ['Abrahamson\n2015SSlab', 'AtkBoore\n2003SSlab', 'Garcia\n2005SSlab', 'MegaPan\n2010']
+banda_gmm_inslab_w = get_weights(banda_gmm_inslab_qlist, weighted_sum)
+print banda_gmm_inslab_w, sum(banda_gmm_inslab_w)
+banda_gmm_inslab_region_w = banda_gmm_inslab_w *banda_gmm_region_w[4]
+banda_gmm_model_all_w += list(banda_gmm_inslab_region_w)
+banda_gmm_model_all_labels += banda_gmm_inslab_labels
+bar_plot(banda_gmm_inslab_qlist, banda_gmm_inslab_w, banda_gmm_inslab_labels, \
+         'banda_gmm_inslab_weights.png', \
+         'Banda Sea Intraslab GMM Weights', fig_path = gmm_fig_path)
+
+##########################
+# GMM Cutoff distances
+# Cratonic, Non-Cratonic and Extended cutoff distances
+c_nc_ex_cutoff_inslab_qlist = ['S5Q1', 'S5Q2', 'S5Q3', 'S5Q4', 'S5Q5']
+c_nc_ex_cutoff_inslab_labels = ['200', '300', '400', '500', '600']
+c_nc_ex_cutoff_inslab_w = get_weights(c_nc_ex_cutoff_inslab_qlist, weighted_sum)
+print c_nc_ex_cutoff_inslab_w, sum(c_nc_ex_cutoff_inslab_w)
+bar_plot(c_nc_ex_cutoff_inslab_qlist, c_nc_ex_cutoff_inslab_w, c_nc_ex_cutoff_inslab_labels, \
+         'c_nc_ex_cutoff_distance_weights.png', \
+         'Cratonic, Non-cratonic and Extended Cutoff Distance Weights', fig_path = gmm_fig_path)
+
+# Banda Sea cutoff distances
+banda_cutoff_inslab_qlist = ['S5Q6', 'S5Q7', 'S5Q8', 'S5Q9']
+banda_cutoff_inslab_labels = ['500', '600', '800', '1000']
+banda_cutoff_inslab_w = get_weights(banda_cutoff_inslab_qlist, weighted_sum)
+print banda_cutoff_inslab_w, sum(banda_cutoff_inslab_w)
+bar_plot(banda_cutoff_inslab_qlist, banda_cutoff_inslab_w, banda_cutoff_inslab_labels, \
+         'banda_cutoff_distance_weights.png', \
+         'Banda Sea Cutoff Distance Weights', fig_path = gmm_fig_path)
+
+#################################
+# Plot full model weights 
+bar_plot(banda_gmm_model_all_w, banda_gmm_model_all_w, banda_gmm_model_all_labels, \
+         'banda_gmm_all_weights.png', \
+         'Banda Sea All Model Weights', fig_path = gmm_fig_path, fontsize=10, wide=2.5)
+bar_plot(c_gmm_model_all_w, c_gmm_model_all_w, c_gmm_model_all_labels, \
+         'cratonic_gmm_all_weights.png', \
+         'Cratonic All Model Weights', fig_path = gmm_fig_path, fontsize=10, wide=2)
+bar_plot(nc_ex_gmm_model_all_w, nc_ex_gmm_model_all_w, nc_ex_gmm_model_all_labels, \
+         'noncratonic_extended_gmm_all_weights.png', \
+         'Non-cratonic and Extended All Model Weights', fig_path = gmm_fig_path, fontsize=10, wide=2)
