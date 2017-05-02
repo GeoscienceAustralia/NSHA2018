0f3c964720025520d7f3be165d2be7ce0dd28960
Jonathan Griffin <jono.d.griffin@gmail.com>
2017-05-02 16:49:41 +1000

    Fixed error not dealing with variable job.ini filenames

--- jobs/run_oq_model.py
+++ jobs/run_oq_model.py
@@ -101,7 +101,7 @@ outlines += '#PBS -l other=hyperthread\n\n'
 
 outlines += 'module load openquake/2.1.1\n'
 outlines += 'oq-ini.all.sh\n'
-outlines += 'oq engine --run job.ini --exports csv >&  parjob.log\n'
+outlines += 'oq engine --run %s --exports csv >&  parjob.log\n' % params['job_file']
 outlines += 'oq-end.sh'
 
 run_script_name = 'run_%s.sh' % model_name
