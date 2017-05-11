86998ab0e073b1bbdab0dcf18069f216e61d707a
treviallen <trevor.allen@canada.ca>
2017-05-11 22:24:28 +1000

    updating ARUP shapes

--- source_models/logic_trees/make_source_model_logictree.py
+++ source_models/logic_trees/make_source_model_logictree.py
@@ -13,20 +13,22 @@ lt = LogicTree('../../shared/seismic_source_model_weights_rounded_p0.4.edit.csv'
 
 # get list of source files from area sources
 xmllist = []
+xmlpath = []
 rootfolder = path.join('..', 'zones', '2012_mw_ge_4.0' )
 for root, dirnames, filenames in walk(rootfolder):
     #for filename in filter(filenames, '.pdf'):
     for filename in filenames:
         if filename.endswith('_collapsed.xml') and path.split(root)[-1] == 'collapsed':
             xmllist.append(filename)
+            xmlpath.append(root)
 
 
 # set up metadata dictionary
-modelPath = getcwd()
+modelPath = getcwd() # path where source logit tree is to be saved
 meta = {'modelPath': modelPath, 'modelFile':'nsha18_source_model_logic_tree.xml'}
 
 # get set weights
-
+src_type, src_wts = lt.get_weights('Source_model', 'Source_type')
 
 # now get model weights
 bak_mods, bak_wts = lt.get_weights('Source_model', 'Background')
@@ -35,6 +37,15 @@ sms_mods, sms_wts = lt.get_weights('Source_model', 'Smoothed_seismicity')
 smo_mods, smo_wts = lt.get_weights('Source_model', 'Seismotectonic')
 smf_mods, smf_wts = lt.get_weights('Source_model', 'Smoothed_seismicity') # w/ faults
 
-
+# loop throgh source model types
+for st, sw in zip(src_type, src_wts):
+    print st
+    # assume smoothed faults same weight as smoothed seis
+    if st == 'Smoothed_faults':
+        st = 'Smoothed_seismicity'
+        
+    # get weights within source type
+    models, weights = lt.get_weights('Source_model', st)
+    print models, weights
 
 #make_logic_tree(srcxmls, branch_wts, meta)
