d76a371d25402a060f13c7921534072f989084c5
treviallen <trevor.allen@canada.ca>
2017-05-11 23:22:20 +1000

    updating logic tree writer code

--- source_models/logic_trees/make_source_model_logictree.py
+++ source_models/logic_trees/make_source_model_logictree.py
@@ -5,7 +5,7 @@ Created on Thu May 11 17:41:07 2017
 @author: u56903
 """
 from os import path, getcwd, walk
-#from tools.make_nsha_oq_inputs import make_logic_tree
+from tools.make_nsha_oq_inputs import make_logic_tree
 from logic_tree import LogicTree
 
 # parse expert elicitation weights
@@ -16,7 +16,6 @@ xmllist = []
 xmlpath = []
 rootfolder = path.join('..', 'zones', '2012_mw_ge_4.0' )
 for root, dirnames, filenames in walk(rootfolder):
-    #for filename in filter(filenames, '.pdf'):
     for filename in filenames:
         if filename.endswith('_collapsed.xml') and path.split(root)[-1] == 'collapsed':
             xmllist.append(filename)
@@ -30,22 +29,59 @@ meta = {'modelPath': modelPath, 'modelFile':'nsha18_source_model_logic_tree.xml'
 # get set weights
 src_type, src_wts = lt.get_weights('Source_model', 'Source_type')
 
-# now get model weights
-bak_mods, bak_wts = lt.get_weights('Source_model', 'Background')
-reg_mods, reg_wts = lt.get_weights('Source_model', 'Regional')
-sms_mods, sms_wts = lt.get_weights('Source_model', 'Smoothed_seismicity')
-smo_mods, smo_wts = lt.get_weights('Source_model', 'Seismotectonic')
-smf_mods, smf_wts = lt.get_weights('Source_model', 'Smoothed_seismicity') # w/ faults
+# set branch weights
+branch_wts = []
+branch_files = []
 
 # loop throgh source model types
 for st, sw in zip(src_type, src_wts):
-    print st
+
     # assume smoothed faults same weight as smoothed seis
     if st == 'Smoothed_faults':
         st = 'Smoothed_seismicity'
         
     # get weights within source type
-    models, weights = lt.get_weights('Source_model', st)
-    print models, weights
+    models, mod_wts = lt.get_weights('Source_model', st)
+    
+    # now loop through models within source type
+    for mod, mw in zip(models, mod_wts):
+        
+        # loop through xml files and find matches
+        for xl, xp in zip(xmllist, xmlpath):
+            
+            if xl.upper().startswith(mod.upper()):
+            	
+                # do a couple of checks
+                if mod == 'NSHA13' and xl.upper().startswith('NSHA13_BACKGROUND'):
+                    print 'Not adding '+xl+' to '+st+' set'
+                
+                elif mod == 'DIMAUS' and st == 'Seismotectonic':
+                    print 'Not adding '+xl+' to '+st+' set'
+                    
+                elif mod == 'AUS6' and st == 'Seismotectonic':
+                    print 'Not adding '+xl+' to '+st+' set'
+                
+                # else, add file to list
+                else:
+                    # multiply ARUP models by 0.5
+                    if mod.startswith('ARUP'):
+                       mod_wt = 0.5
+                       print 'Modifying ARUP model'
+                    else:
+                       mod_wt = 1.0
+                       
+                    # now get final model weights
+                    branch_wts.append(mod_wt * mw * sw)
+                    
+                    # add model paths
+                    branch_files.append(path.join(xp, xl))
+                
+# do Jono's rounding thingy here...
 
-#make_logic_tree(srcxmls, branch_wts, meta)
+# check weights sum to one!
+if not sum(branch_wts) == 1.0:
+    print '\nWeights do not sum to 1.0!'
+
+# write source model logic tree
+print '\nUse full file paths!!!'
+make_logic_tree(branch_files, branch_wts, meta)
