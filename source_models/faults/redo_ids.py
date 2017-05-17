9d6603883366855280a20b4787f222f788ab823f
Jonathan Griffin <jono.d.griffin@gmail.com>
2017-05-17 16:46:32 +1000

    Helper script for renumbering source ids after parallel source generation gives some of them the same id

---
+++ source_models/faults/redo_ids.py
@@ -0,0 +1,27 @@
+""" script to update source ids in case multiple sources have the same id
+"""
+import os, sys
+
+def number_ids(source_file, prefix = ''):
+    """Main function
+    """
+    f_in = open(source_file, 'r')
+    new_source_file = source_file[:-4] + '_new_ids.xml'
+    f_out = open(new_source_file, 'w')
+    index = 1
+    for line in f_in.readlines():
+        if line.lstrip().startswith(r'id="'):
+            new_line = '        id="%s_%06d"\n' % (prefix, index)
+            index += 1
+            f_out.write(new_line)
+        else:
+            f_out.write(line)
+        
+    f_in.close()
+    f_out.close()
+
+if __name__ == "__main__":
+    pathname = '/short/w84/NSHA18/sandpit/jdg547/NSHA2018/source_models/zones/2012_mw_ge_4.0/AUS6/input/collapsed'
+    source_filename = 'AUS6_collapsed_pts_geom_weighted_merged_parallel.xml'
+    source_file = os.path.join(pathname, source_filename)
+    number_ids(source_file, prefix = 'NSHA13GF')
