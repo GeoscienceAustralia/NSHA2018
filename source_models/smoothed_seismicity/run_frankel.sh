d728f3b8e1dedf3c6bb0e04568ce051af69dab05
Jonathan Griffin <jono.d.griffin@gmail.com>
2017-11-07 16:25:13 +1100

    Updating commits

--- source_models/smoothed_seismicity/run_frankel.sh
+++ source_models/smoothed_seismicity/run_frankel.sh
@@ -1,7 +1,7 @@
 #PBS -P w84
 #PBS -q express
 #PBS -l walltime=16:00:00
-#PBS -l ncpus=9
+#PBS -l ncpus=10
 #PBS -l mem=32GB
 #PBS -l wd
 
@@ -20,6 +20,7 @@ export SPATIALINDEX_C_LIBRARY=/short/n74/jdg547/spatialindex-src-1.8.5/lib/libsp
 export LD_LIBRARY_PATH=/short/n74/jdg547/spatialindex-src-1.8.5/lib:$LD_LIBRARY_PATH
 # Python paths for local openquake installs and dependencies
 export PYTHONPATH=.:/home/547/jdg547/.local/lib/python2.7/site-packages:${PYTHONPATH}
+export PYTHONPATH=.:/short/n74/src/lib/python:${PYTHONPATH}
 export PYTHONPATH=.:/short/w84/NSHA18/sandpit/jdg547/oq-hazardlib:${PYTHONPATH}
 export PYTHONPATH=.:/short/w84/NSHA18/sandpit/jdg547/oq-engine:${PYTHONPATH}
 export PYTHONPATH=.:/short/w84/NSHA18/sandpit/jdg547/hmtk:${PYTHONPATH}
@@ -28,13 +29,13 @@ export PYTHONPATH=.::/short/w84/NSHA18/sandpit/jdg547/:${PYTHONPATH}
 
 counter=0
 one=1
-bvals=(0.779 0.835 1.198 0.708 0.727 1.043 0.850 0.944 1.352) # declustered
+bvals=(0.779 0.835 1.198 0.708 0.727 1.043 0.850 0.944 1.352 1.0) # declustered
 #bvals=(1.016 0.872 1.221 0.963 0.763 1.066 1.068 0.981 1.375) # full catalogue
 for bval in ${bvals[*]}; do
-    python fixed_smoothing.py $bval > frankel_b$bval.log &
+    python fixed_smoothing_aees_testing.py $bval > frankel_b$bval.log &
     counter=$(($counter+$one));
     # Once we have submitted jobs for all 9 bvalues, break from this loop.
-    if [ $counter = 9 ];
+    if [ $counter = 10 ];
     then
         break
     fi
