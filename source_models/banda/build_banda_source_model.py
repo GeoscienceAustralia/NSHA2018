7ba47911a3068717b20ba62286bd5ccf4e50824e
Jonathan Griffin <jono.d.griffin@gmail.com>
2017-11-17 15:04:58 +1100

    Initial fault model for Northern Australia sourcesq

--- source_models/banda/build_banda_source_model.py
+++ source_models/banda/build_banda_source_model.py
@@ -15,7 +15,7 @@ except ImportError:
     print 'NetCDF4 not installed/loaded, be sure to run module load netcdf on NCI'
 
 contour_dir = './shp'
-contour_shapefiles = glob(os.path.join(contour_dir, 'sunda.shp'))
+contour_shapefiles = glob(os.path.join(contour_dir, '*.shp'))
 rate_dir = './source_rates'
 source_model_name = 'Banda_Sources_NSHA_2018'
 tectonic_region = 'banda'
@@ -31,6 +31,22 @@ output_xml = []
 shp2nrml.append_xml_header(output_xml, source_model_name)
 
 java_limit=104.8 # longitude; western limit of java segment, to clip contours by
+source_mmin_dict = {'arutrough':6.5,
+                'banda_detachment':7.0,
+                'flores':7.0,
+                'hjort':6.5,
+                'macquarienorth':5.5,
+                'moresby_trough':5.5,
+                'newguinea':7.0,
+                'puysegur':6.7,
+                'seram_thrust':7.0,
+                'solomon':7.5,
+                'sunda':8.0,
+                'tanimbar':6.5,
+                'timor':6.5,
+                'trobriand':7.0}
+
+
 
 # Now add each fault source
 id_base = 'banda_'
@@ -54,49 +70,21 @@ for shapefile in contour_shapefiles:
                                                      contours)
 
     # Now we want to read and sum the rates from the PTHA data
-    # to get cumulative rates
-#    ratefilename = 'all_uniform_slip_earthquake_events_%s.nc' % sourcename
-#    ratefile = os.path.join(rate_dir, ratefilename)
-#    ratedata = netCDF4.Dataset(ratefile, 'r')
-#    magnitudes = ratedata.variables['Mw'][:]
-    ratefilename = 'EVENT_RATES/output_mw_rate_curve_%s.csv' % sourcename
+    if sourcename == 'sunda': # only want rates for java segment
+        ratefilename = 'EVENT_RATES/output_mw_rate_curve_sunda_java.csv'
+    else:
+        ratefilename = 'EVENT_RATES/output_mw_rate_curve_%s.csv' % sourcename
     ratedata = np.genfromtxt(ratefilename, delimiter=',', skip_header=1)
-    min_mag = ratedata[0][0]
+    # Get rates above Mmin
+    min_mag = source_mmin_dict[sourcename]
     rates = ratedata[:,1]
+    magnitudes = ratedata[:,0]
+    rates = rates[np.argwhere(magnitudes >= min_mag)].flatten()
     print min_mag
     print rates
-#    rates = ratedata.variables['rate_annual'][:]
-#    mw_rates = np.vstack([magnitudes, rates])
-    
-
-    # These should be sorted by magnitude, but best not to assume
-#    mw_rates = mw_rates[mw_rates[:,1].argsort()]
-#    mw_rates = np.fliplr(mw_rates) # Descending order for cum-sum calcs
-#    mw_list = []
-#    cum_rates = []
-#    rate_cumsum = 0
-#    mag = mw_rates[1][0]
-
-#    j=0
-#    for mw in mw_rates[1][:]:
-#        if mw == mag:
-#            rate_cumsum += mw_rates[0][j]
-#        else:
-#            cum_rates.append(rate_cumsum)
-#            mw_list.append(mag)
-#            mag = mw_rates[1][j] # Update to next magnitude increment
-#            rate_cumsum += mw_rates[0][j]
-#        j+=1
-
-    # Append final rates
-#    mw_list.append(mag)
-#    cum_rates.append(rate_cumsum)
-    # Reverse rates to start at min mag
-#    cum_rates = cum_rates[::-1]
-    # remove rates = 0
-#    cum_rates = list(filter((0.0).__ne__, cum_rates))
-#    min_mag = min(mw_list)    
-
+    # Get rid of zero valued rates
+    rates = rates[np.argwhere(rates > 0)].flatten()
+    print rates
     shp2nrml.append_incremental_mfd(output_xml, magnitude_scale_rel,
                                     rupture_aspect_ratio, rake,
                                     min_mag, bin_width, rates)
