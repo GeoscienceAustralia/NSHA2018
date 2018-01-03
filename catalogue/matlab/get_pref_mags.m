5ac2a20ca820b2669ad9892c181a2a4f6e7f9411
treviallen <trevor.allen@ga.gov.au>
2018-01-03 13:50:28 +1100

    adding get_pref_mags.m

---
+++ catalogue/matlab/get_pref_mags.m
@@ -0,0 +1,70 @@
+% scropt to find preferred magnitude of each magnitude type
+
+if exist('mdat','var') ~= 1
+    disp('Loading mdat');
+    load mdat_mw.mat;
+end
+
+%% loop thru all events
+for i = 1:length(mdat)
+    % find preferred ML
+    if ~isnan(mdat(i).Allen_ML)
+        mdat(i).MDAT_prefML = mdat(i).Allen_ML;
+        mdat(i).MDAT_prefMLSrc = 'Allen (unpublished)';
+    elseif ~isnan(mdat(i).ANSN_ml)
+        mdat(i).MDAT_prefML = mdat(i).ANSN_ml;
+        mdat(i).MDAT_prefMLSrc = 'AUST';
+    elseif strcmp(mdat(i).GG_Mtype, 'ML')
+        mdat(i).MDAT_prefML = mdat(i).GG_Mval;
+        mdat(i).MDAT_prefMLSrc = mdat(i).MDAT_locsrc;
+    else
+        mdat(i).MDAT_prefML = NaN;
+        mdat(i).MDAT_prefMLSrc = '';
+    end
+    
+    % find preferred MW
+    if ~isnan(mdat(i).altMW)
+        mdat(i).MDAT_prefMW = mdat(i).altMW;
+        mdat(i).MDAT_prefMWSrc = mdat(i).altMWsrc;
+    elseif ~isnan(mdat(i).ANSN_mw)
+        mdat(i).MDAT_prefMW = mdat(i).ANSN_mw;
+        mdat(i).MDAT_prefMWSrc = 'AUST';
+    elseif strcmp(mdat(i).GG_Mtype, 'MW')
+        mdat(i).MDAT_prefMW = mdat(i).GG_Mval;
+        mdat(i).MDAT_prefMWSrc = mdat(i).MDAT_locsrc;
+    elseif ~isnan(mdat(i).ANSN_mwp)
+        mdat(i).MDAT_prefMW = mdat(i).ANSN_mwp;
+        mdat(i).MDAT_prefMWSrc = 'AUST';
+    else
+        mdat(i).MDAT_prefMW = NaN;
+        mdat(i).MDAT_prefMWSrc = '';
+    end
+
+    % find preferred MS
+    if strcmp(mdat(i).GG_Mtype, 'MS')
+        mdat(i).MDAT_prefMS = mdat(i).GG_Mval;
+        mdat(i).MDAT_prefMSSrc = mdat(i).MDAT_locsrc;
+    elseif ~isnan(mdat(i).ANSN_ms)
+        mdat(i).MDAT_prefMS = mdat(i).ANSN_ms;
+        mdat(i).MDAT_prefMSSrc = 'AUST';
+     else
+        mdat(i).MDAT_prefMS = NaN;
+        mdat(i).MDAT_prefMSSrc = '';
+    end
+    
+    % find preferred mb
+    if strcmp(mdat(i).GG_Mtype, 'mb')
+        mdat(i).MDAT_prefmb = mdat(i).GG_Mval;
+        mdat(i).MDAT_prefmbSrc = mdat(i).MDAT_locsrc;
+    elseif ~isnan(mdat(i).ANSN_mb)
+        mdat(i).MDAT_prefmb = mdat(i).ANSN_mb;
+        mdat(i).MDAT_prefmbSrc = 'AUST';
+     else
+        mdat(i).MDAT_prefmb = NaN;
+        mdat(i).MDAT_prefmbSrc = '';
+    end
+    
+end
+
+% save mat file
+save mdat_pref_mag_types mdat
