f14386c5a8aa8a536b7d438b9750da3f49fada08
treviallen <trevor.allen@ga.gov.au>
2018-03-22 13:26:54 +1100

    kludge to combine source models

--- source_models/smoothed_seismicity/combine_ss_models.py
+++ source_models/smoothed_seismicity/combine_ss_models.py
@@ -138,8 +138,13 @@ def combine_ss_models(filename_stem, domains_shp, params,lt, bval_key, output_di
             filename_stem, dom[bval_key], mmin,
             completeness_string)
         print 'Parsing %s' % filename
+        
+        # TA kluge - hardwire jdg547 path
+        jdgpath = '/short/w84/NSHA18/sandpit/jdg547/NSHA2018/source_models/smoothed_seismicity/'                
+        
         # Only keep points within domain
-        pts = read_pt_source(filename)
+        pts = read_pt_source(jdgpath+filename)
+        
 #        shapes = np.where(trt_types
         for shape in dsf.shapeRecords():
 #            print code_index
