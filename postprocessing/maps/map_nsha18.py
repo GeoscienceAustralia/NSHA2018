d16bcc95a4c11dc84f18239e7582c7b5580ae0b2
treviallen <trevor.allen@canada.ca>
2018-03-26 23:12:14 +1100

    resetting 2% colour scale

--- postprocessing/maps/map_nsha18.py
+++ postprocessing/maps/map_nsha18.py
@@ -101,7 +101,7 @@ for line in lines[2:]:
 #keys = ['PGA_10', 'PGA_02', 'SA02_10', 'SA02_02', 'SA10_10', 'SA10_02']
 #plt.clf()
 #plt.cla()
-for i, key in enumerate([keys[0]]): # just plot 1 for now!
+for i, key in enumerate([keys[1]]): # just plot 1 for now!
     
     # get IM period
     period = key.split('-')[0]
@@ -251,7 +251,7 @@ for i, key in enumerate([keys[0]]): # just plot 1 for now!
     #cmap = cm.rainbow
     if period == 'PGA':
         
-        if probability == '10%':
+        if probability == '10%' or probability == '2%': # kluge to get on same scale
             ncolours = 13
             vmin = -2.
             vmax = -0.25 - 0.125 # so there is an odd number for which to split the cpt
