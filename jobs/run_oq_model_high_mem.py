17149b6808051442a34e074a27ea6e72f4ad86ff
treviallen <trevor.allen@canada.ca>
2018-08-02 23:33:27 +1000

    adding new log

--- jobs/run_oq_model_high_mem.py
+++ jobs/run_oq_model_high_mem.py
@@ -109,6 +109,7 @@ outlines += 'module load openquake/3.1\n'
 outlines += 'oq-ini.all.sh\n'
 outlines += 'oq engine info %s >& info.log\n' % params['job_file']
 outlines += 'oq engine --run %s --exports csv >&  parjob.log\n' % params['job_file']
+outlines += 'oq engine --lhc >&  lhc.log\n'
 outlines += 'oq engine --run job_stats.ini --hc 1 --exports csv >&  jobstats.log\n'
 outlines += 'oq-end.sh'
 
