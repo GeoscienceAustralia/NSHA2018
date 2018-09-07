23e4bf44735227acffa78bb8de0e98e1e1294cb7
treviallen <trevor.allen@ga.gov.au>
2018-09-07 17:54:15 +1000

    updating interp_nsha18_hazgrids.py

--- source_models/complete_model/final/interp_nsha18_hazgrids.py
+++ source_models/complete_model/final/interp_nsha18_hazgrids.py
@@ -5,6 +5,9 @@ from sys import argv
 
 probs = array([0.02,0.01375,0.01,0.00445,0.002,0.0021,0.001,0.0005,0.000404,0.0002,0.0001])
 
+periods = ['PGA', 'SA(0.05)', 'SA(0.07)', 'SA(0.1)', 'SA(0.2)', 'SA(0.3)', 'SA(0.4)', \
+           'SA(0.1)']
+
 hazcurvefile = argv[1]
 
 # check to see if maps exists
@@ -32,7 +35,9 @@ for site in siteDict:
 # make output
 ###############################################################################
 
-outcsv = path.join('hazard_grids', 'interp_haz_curves.csv')
+outfile = 
+
+outpath = path.join('hazard_grids', 'interp_haz_curves.csv')
 f = open(outcsv, 'wb')
 f.write(interpTXT)
 f.close()
